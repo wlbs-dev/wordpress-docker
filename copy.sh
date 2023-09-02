3ec3264138aad150bceb6349b9e2a02e015c74af
Anshul Laikar <anshullaikar@gmail.com>
2023-09-02 13:07:57 +0530

    Fix --s3 flag handling in backup copy script

--- copy.sh
+++ copy.sh
@@ -23,7 +23,7 @@ DATE=$(date +"%Y_%m_%d_%H_%M_%S")
 SOURCE_PATH="/root/old_old_db.sql"
 
 # Destination path on your host machine
-DEST_PATH="backup_docker_${DATE}.sql"your_default_s3_folder
+DEST_PATH="backup_docker_${DATE}.sql"
 
 # Get the full container ID
 FULL_CONTAINER_ID=$(docker compose ps -q $SERVICE_NAME)
@@ -39,23 +39,20 @@ mv ${DEST_PATH} backup.sql
 
 echo "File renamed successfully."
 
-# Ask user if they want to push backup to S3
-# Allow user to pass flag --s3 to skip this prompt
+# Check if the --s3 flag is provided
 if [[ $1 == "--s3" ]]; then
     echo "Skipping prompt to push backup to S3."
-    $REPLY="y"
-    exit 0
+    REPLY="y"
 else
     echo "Prompting user to push backup to S3."
     read -p "Do you want to push backup to S3? (y/n): " -n 1 -r
     echo    # Move to a new line
 fi
 
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
+if [[ $REPLY =~ ^[Yy]$ ]]; then
     # Folder should be set in environment variable
     # Upload to S3 using aws-cli
-    # if $URL contains localhost call it local_backup
+    # if $URL contains localhost, call it local_backup
     if [[ $URL == *"localhost"* ]]; then
         aws s3 cp backup.sql "s3://${S3_BACKUP_LOCATION}/local_backup_${DATE}.sql"
     else
