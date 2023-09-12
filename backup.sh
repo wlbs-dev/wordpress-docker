cafd338bb37c93ba6128d78038eb2e65e091e923
Anshul Laikar <anshullaikar@gmail.com>
2023-09-12 13:29:14 +0530

    Mount .env into backup container and clean up backup script

--- backup.sh
+++ backup.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
 set -e
 
-source .env
+source /.env
 
 # Create backup script
 cat <<EOF > /run_backup.sh
 #!/bin/sh
 date_time=\$(date +"%Y%m%d%H%M%S")
-mysqldump -h\$MYSQL_HOST -u\$MYSQL_USER -p\$MYSQL_PASSWORD \$MYSQL_DATABASE orders > /db_backup/orders_\$date_time.sql
+mysqldump -h${MYSQL_HOST} -u${MYSQL_USER} -p${MYSQL_PASSWORD} ${MYSQL_DATABASE} orders > /db_backup/orders_\$date_time.sql
 
-aws s3 cp /db_backup/orders_\$date_time.sql s3://\$S3_BUCKET/orders_\$date_time.sql
-
+aws s3 cp /db_backup/orders_\$date_time.sql s3://${S3_BUCKET}/orders_\$date_time.sql
 EOF
 
 # Make it executable
